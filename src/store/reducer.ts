5fa96b14757b0c0dec8952dc735dbf48e9319023
Z-HNAN <zhn68033@163.com>
2020-04-21 18:48:39 +0800

    App feat 引入redux

---
+++ src/store/reducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+
+// import homeReducer from '../pages/Home/reducer'
+
+
+export default combineReducers({
+  // home: homeReducer,
+})
---
+++ src/store/saga.ts
@@ -0,0 +1,9 @@
+import { all } from 'redux-saga/effects'
+
+// import home from '../pages/Home/saga'
+
+export default function* () {
+  yield all([
+    // home(),
+  ])
+}
