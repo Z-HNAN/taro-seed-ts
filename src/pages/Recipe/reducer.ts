8695c2204dc613ee9aa310f1d925bffb60f0ee39
Z-HNAN <zhn68033@163.com>
2020-04-22 15:28:21 +0800

    feat 增加项目页面骨架

---
+++ src/pages/Recipe/reducer.ts
@@ -0,0 +1,15 @@
+// import { } from './actions'
+
+export interface StateType {
+
+}
+
+const INITIAL_STATE: StateType = {
+}
+
+export default function reducer(state = INITIAL_STATE, action): StateType {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
